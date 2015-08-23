91b2b0279503a58800948a7afe6767b80043bf61
paarandika <paarandika@gmail.com>
2015-08-23 14:36:46 +0530

    ui

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,23 @@
-var online=true;
-function appCtrl($scope){
-  $scope.d=0;
-}
+var online = true;
+var app = angular.module("GI-app", []);
+app.controller("appCtrl", function($scope) {
+  $scope.search = true;
+  $scope.results = data;
+  $scope.getMyCtrlScope = function() {
+    return $scope;
+  }
+  $scope.currentResult;
+
+});
+
+var data = [{
+  "id": "1",
+  "name": "rand",
+  "salary": "34.5",
+  "update_t": "1440256944"
+}, {
+  "id": "2",
+  "name": "nemo",
+  "salary": "340",
+  "update_t": "1440312486"
+}];
