04772190fb1a4fcd883e818b498f83aa3361a519
paarandika <paarandika@gmail.com>
2015-08-23 18:32:20 +0530

    offline finished

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,36 @@
 var online = true;
 var app = angular.module("GI-app", []);
-app.controller("appCtrl", function($scope) {
+app.controller("appCtrl", function($scope, $http) {
   $scope.search = true;
-  $scope.results = data;
+  $scope.results = [];
+  $scope.searchText = "";
   $scope.getMyCtrlScope = function() {
     return $scope;
   }
-  $scope.currentResult;
+  $scope.currentResult = {};
+  $scope.searchUsers = function() {
+    if (online) {
+      $.post("http://192.168.0.102/mock/search.php", {
+          name: $scope.searchText
+        },
+        function(d, status) {
+          $scope.results = jQuery.parseJSON(d);
+          $scope.$apply();
+        });
+    }
+  }
+
+  $scope.submitUser = function() {
+    $.post("http://192.168.0.102/mock/add.php", {
+        name: $scope.currentResult.name,
+        id: $scope.currentResult.id,
+        salary: $scope.currentResult.salary
+      },
+      function(d, status) {
+        alert(d);
+        // $scope.results[$scope.currentResult.id] = jQuery.parseJSON(d);
+        // $scope.$apply();
+      });
+  }
 
 });
-
-var data = [{
-  "id": "1",
-  "name": "rand",
-  "salary": "34.5",
-  "update_t": "1440256944"
-}, {
-  "id": "2",
-  "name": "nemo",
-  "salary": "340",
-  "update_t": "1440312486"
-}];
